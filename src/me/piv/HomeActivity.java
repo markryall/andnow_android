dcf207baf83b188594c650bcb7e6a7ee4e3d0354
Mark Ryall <mark@ryall.name>
2012-02-03 16:20:37 +1000

    added some custom button colours

--- src/me/piv/HomeActivity.java
+++ src/me/piv/HomeActivity.java
@@ -2,14 +2,29 @@ package me.piv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 
-public class HomeActivity extends Activity
-{
+public class HomeActivity extends Activity implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        findViewById(R.id.start_button).setOnClickListener(this);
+        findViewById(R.id.stop_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.start_button:
+                Log.i("me.piv.HomeActivity","started");
+                return;
+            case R.id.stop_button:
+                Log.i("me.piv.HomeActivity","stopped");
+                return;
+        }
     }
 }
