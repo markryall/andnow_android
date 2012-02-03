70c1ce8b4b8e69bfe319b43c82fadd468aaf30f7
Mark Ryall <mark@ryall.name>
2012-02-03 15:44:24 +1000

    added a couple of buttons to main activity

--- src/me/piv/HomeActivity.java
+++ src/me/piv/HomeActivity.java
@@ -2,14 +2,14 @@ package me.piv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class HomeActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
     }
 }
