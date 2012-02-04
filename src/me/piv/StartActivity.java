aac836e690fcb20e4ebdbb2c09bc96473a529721
Mark Ryall <mark@ryall.name>
2012-02-04 14:42:11 +1000

    added text entry of description to capture the start of an actity

--- src/me/piv/StartActivity.java
+++ src/me/piv/StartActivity.java
@@ -1,15 +1,44 @@
 package me.piv;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import me.piv.data.SessionData;
 
-public class StartActivity extends Activity {
+import static me.piv.data.SessionColumns.TABLE_NAME;
+import static me.piv.data.SessionColumns.START_TIME;
+import static me.piv.data.SessionColumns.DESCRIPTION;
+
+public class StartActivity extends Activity implements View.OnClickListener {
+    private SessionData sessions;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.start);
+        sessions = new SessionData(this);
         Log.i("me.piv.StartActivity", "started");
+        findViewById(R.id.ok).setOnClickListener(this);
+    }
+
+    public void createSession(String description) {
+        Log.i("me.piv.StartActivity",description);
+        SQLiteDatabase db = sessions.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(START_TIME, System.currentTimeMillis());
+        values.put(DESCRIPTION, description);
+        db.insertOrThrow(TABLE_NAME, null, values);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText edittext = (EditText) findViewById(R.id.description);
+        createSession(edittext.getText().toString());
+        this.finish();
     }
 }
