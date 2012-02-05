59442ac33c8e7419504af0f955ad916f17284bd5
Mark Ryall <mark@ryall.name>
2012-02-05 21:45:18 +1000

    moved any database logic out of stop activity and added cursor management to activity

--- src/me/piv/andnow/activity/Stop.java
+++ src/me/piv/andnow/activity/Stop.java
@@ -1,9 +1,6 @@
 package me.piv.andnow.activity;
 
 import android.app.ListActivity;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -11,57 +8,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import me.piv.andnow.R;
 import me.piv.andnow.data.Session;
-import me.piv.andnow.data.SessionData;
-
-import java.util.ArrayList;
+import me.piv.andnow.data.SessionRepository;
 import java.util.List;
 
-import static android.provider.BaseColumns._ID;
-import static me.piv.andnow.data.Session.TABLE_NAME;
-import static me.piv.andnow.data.Session.START_TIME;
-import static me.piv.andnow.data.Session.END_TIME;
-import static me.piv.andnow.data.Session.DESCRIPTION;
-
 public class Stop extends ListActivity implements AdapterView.OnItemClickListener {
-    private static String[] FROM = { _ID, START_TIME, DESCRIPTION };
-    private static String ORDER_BY = START_TIME + " DESC";
-    private static String WHERE = END_TIME + " IS NULL";
-
-    private SessionData sessionData;
-    private List sessions;
+    private SessionRepository sessionRepository;
+    private List<Session> sessions;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        sessionData = new SessionData(this);
-        populateSessions();
-        getListView().setOnItemClickListener(this);
-    }
-
-    private void populateSessions() {
-        Cursor cursor = getIncompleteSessions();
-        sessions = new ArrayList<Session>();
-        while (cursor.moveToNext()) {
-            Session session = new Session(cursor.getLong(0), cursor.getLong(1), 0, cursor.getString(2));
-            sessions.add(session);
-        }
+        sessionRepository = new SessionRepository(this);
+        sessions = sessionRepository.getIncompleteSessions();
         setListAdapter(new ArrayAdapter(this, R.layout.stop_list_item, sessions));
-    }
-
-    private Cursor getIncompleteSessions() {
-        SQLiteDatabase db = sessionData.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, FROM, WHERE, null, null, null, ORDER_BY);
-        startManagingCursor(cursor);
-        return cursor;
+        getListView().setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        SQLiteDatabase db = sessionData.getWritableDatabase();
-        Session session = (Session)sessions.get(i);
-        ContentValues values = new ContentValues();
-        values.put(END_TIME, System.currentTimeMillis());
-        db.update(TABLE_NAME, values, "_ID = " + session.getId(), null);
+        sessionRepository.end(sessions.get(i));
         finish();
     }
 }
--- src/me/piv/andnow/data/SessionRepository.java
+++ src/me/piv/andnow/data/SessionRepository.java
@@ -1,8 +1,11 @@
 package me.piv.andnow.data;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,15 +14,21 @@ import static me.piv.andnow.data.Session.*;
 
 public class SessionRepository {
     private static final String[] ALL_COLS = { _ID, START_TIME, END_TIME, DESCRIPTION };
+    private static final String[] ALL_EXCEPT_END = { _ID, START_TIME, DESCRIPTION };
     private static final String[] DESCRIPTION_COL = { DESCRIPTION };
+    private static String WHERE_END_IS_NULL = END_TIME + " IS NULL";
+    private static String ORDER_BY_START_DESC = START_TIME + " DESC";
     private SessionData sessionData;
+    private Activity activity;
 
-    public SessionRepository(Context context) {
-        sessionData = new SessionData(context);
+    public SessionRepository(Activity activity) {
+        this.activity = activity;
+        sessionData = new SessionData(activity);
     }
 
     public void each(SessionConsumer consumer) {
         Cursor cursor = sessionData.getReadableDatabase().query(TABLE_NAME, ALL_COLS, null, null, null, null, null);
+        activity.startManagingCursor(cursor);
         while (cursor.moveToNext()) {
             consumer.consume(new Session(cursor.getLong(0), cursor.getLong(1), cursor.getLong(2), cursor.getString(3)));
         }
@@ -27,6 +36,7 @@ public class SessionRepository {
 
     public List<String> getUniqueDescriptions() {
         Cursor cursor = sessionData.getReadableDatabase().query(TABLE_NAME, DESCRIPTION_COL, null, null, DESCRIPTION, null, DESCRIPTION);
+        activity.startManagingCursor(cursor);
         List<String> activities = new ArrayList<String>();
         while (cursor.moveToNext()) activities.add(cursor.getString(0));
         return activities;
@@ -37,5 +47,23 @@ public class SessionRepository {
         values.put(START_TIME, start);
         values.put(DESCRIPTION, description);
         return sessionData.getWritableDatabase().insertOrThrow(TABLE_NAME, null, values);
+    }
+
+    public List<Session> getIncompleteSessions() {
+        Cursor cursor = sessionData.getReadableDatabase().query(TABLE_NAME, ALL_EXCEPT_END, WHERE_END_IS_NULL, null, null, null, ORDER_BY_START_DESC);
+        activity.startManagingCursor(cursor);
+        List<Session> sessions = new ArrayList<Session>();
+        while (cursor.moveToNext()) {
+            Session session = new Session(cursor.getLong(0), cursor.getLong(1), 0, cursor.getString(2));
+            sessions.add(session);
+        }
+        return sessions;
+    }
+
+    public void end(Session session) {
+        SQLiteDatabase db = sessionData.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(END_TIME, System.currentTimeMillis());
+        db.update(TABLE_NAME, values, "_ID = " + session.getId(), null);
     }
 }
