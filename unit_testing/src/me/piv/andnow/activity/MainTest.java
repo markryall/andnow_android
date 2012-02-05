7f1709057b607c010a445096d438b03c9af23bd5
Mark Ryall <mark@ryall.name>
2012-02-05 22:15:20 +1000

    added a trivial unit test for json serialisation

---
+++ unit_testing/src/me/piv/andnow/activity/MainTest.java
@@ -0,0 +1,10 @@
+package me.piv.andnow.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import me.piv.andnow.activity.Main;
+
+public class MainTest extends ActivityInstrumentationTestCase2<Main> {
+    public MainTest() {
+        super("me.piv", Main.class);
+    }
+}
---
+++ unit_testing/src/me/piv/andnow/data/SessionTest.java
@@ -0,0 +1,15 @@
+package me.piv.andnow.data;
+
+import junit.framework.TestCase;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SessionTest extends TestCase {
+    public void testShouldConvertToJSON() throws JSONException {
+        Session session = new Session(1, 100, 200, "test");
+        JSONObject json = session.toJSON();
+        assertEquals(json.get("description"), "test");
+        assertEquals(""+json.get("start_time"), "100");
+        assertEquals(""+json.get("end_time"), "200");
+    }
+}
